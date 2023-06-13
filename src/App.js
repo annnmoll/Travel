e2259ebfe14a49a8cc8d837ae25257870d687a71
annnmoll <103569665+annnmoll@users.noreply.github.com>
2023-06-13 19:09:07 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,37 +1,20 @@
 import React, { useEffect, useState } from 'react' ; 
 import { BrowserRouter as Router , Routes , Route } from 'react-router-dom';
 import './App.css';
-import SignIn from './components/SignIn/SignIn'
-import { useDispatch, useSelector } from 'react-redux';
-import { selectUser } from './features/userSlice';
-import { auth } from './firebase';
-import { login } from './features/userSlice';
 import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen' ; 
 import About from './screens/About';
 import Contact from './screens/Contact';
 
 const App = () => {
- const user = useSelector(selectUser) ;
- const dispatch = useDispatch() ;
  
- useEffect(()=>{
-  auth.onAuthStateChanged( user =>{  if (user){
-    dispatch(login({
-      displayName : user.displayName,
-      email : user.email ,
-      photoUrl : user.photoURL ,
-    })
-       )}
-                        }) }  , [])
-
   
   return (
       <Router>
               <Routes >
                  
                 <Route path ='/profile' element={<ProfileScreen /> }/>
-                <Route path ='/' element = { !user ?  <SignIn />  : <HomeScreen />} />
+                <Route path ='/' element = {<HomeScreen /> } />
                 <Route path ='/about' element = {<About />} />
                 <Route path ='/contact' element ={<Contact />} />
 
